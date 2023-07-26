a38b3b73187662a03591d9c1be16462ef8df11a6
Dallas Gale <dallasgale.digital@gmail.com>
2023-07-26 14:21:37 +1000

    wip

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,23 @@
+const PrivacyPage = () => {
+  return (
+    <section className="about-page">
+      <div className="about-page__intro">
+        <h1 className="typography__display--1 typography__color--highlight">
+          PRIVACY
+        </h1>
+        <p className="typography__display--7  typography__color--highlight">
+          Empower the NFT community with unparalleled data insights
+        </p>
+        <p className="typography__display--1">
+          For hobbyists and whales, to web3 consultants and enterprise;
+          DataBeast shows you uniquely helpful insights to ensure a true picture
+          of the ecosystem. We’ve sourced data from the raw events on the
+          blockchain in order to deliver you a comprehensive market overview,
+          without the manipulation.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default PrivacyPage;
